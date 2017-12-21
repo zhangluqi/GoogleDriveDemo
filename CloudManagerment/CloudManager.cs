db53808d5a157d95a8b00cf6e6316c8746546664
张露琪 <zhangluqi@outlook.com>
2017-12-22 00:31:58 +0800

    test2

--- CloudManagerment/CloudManager.cs
+++ CloudManagerment/CloudManager.cs
@@ -20,10 +20,6 @@ namespace CloudManagerment
 {
     public static class CloudManager
     {
-
-
-
-
         private static readonly IList<Cloudbase> Clouds = new List<Cloudbase>();
         
         private static UserInfo userInfo = new UserInfo();
